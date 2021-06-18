527c00e744154ffeddda01652aa257c4bb5a3d2c
Arnav Gosain <arnav@arnavgosain.com>
2021-06-18 21:41:52 +0530

    Init: react router in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { BrowserRouter, Route, Switch, useParams } from "react-router-dom";
+
+const ArticlePage = () => {
+  const { slug } = useParams();
+
+  return <>Slug: {slug}</>;
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {typeof window !== "undefined" && (
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/blog/:slug">
+              <ArticlePage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
